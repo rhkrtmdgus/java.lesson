6271eafc5787ff3a2a9e41a0cb5fa980199bd9e1
sunnhyonn <gustmdrhkr@naver.com>
2022-06-23 07:32:54 +0900

    "6-23 스도쿠 최신화"

--- test/src/test1/Number_01.java
+++ test/src/test1/Number_01.java
@@ -225,16 +225,15 @@ public class Number_01 {
 		
 		aa = 2;
 		while(mm) {
-			cnt = 0;
 			temp1 = rd.nextInt(3);
 			temp2 = rd.nextInt(3);
 			if (adw[temp1][temp2] == 1) {
-				cnt = 1; 
 			}
 			else {
 				adw[temp1][temp2] = aa;
 				mm = false;
 			}
+			
 			
 		}
 		mm = true;
@@ -244,45 +243,37 @@ public class Number_01 {
 		
 		while(mm) {
 			cnt = 0;
-			temp1 = rd.nextInt(3);
-			temp2 = rd.nextInt(3)+3;
 			while(mm) {
-				cnt = 0;
 				temp1 = rd.nextInt(3);
 				temp2 = rd.nextInt(3)+3;
 				if (adw[temp1][temp2] == 1) {
-					cnt = 1; 
 				}
 				else {
 					adw[temp1][temp2] = aa;
 					mm = false;
 				}
-				
 			}
+			
 			mm = true;
 			
 			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2]) {
 				cnt = 1;
 				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
+				
 				temp1 = rd.nextInt(3);
 				
 				while(mm) {
-					cnt = 0;
+					adw[temp1][temp2] = 0;
 					temp1 = rd.nextInt(3);
 					temp2 = rd.nextInt(3)+3;
-					if (adw[temp1][temp2] == 1) {
-						cnt = 1; 
-					}
-					else {
+					if (adw[temp1][temp2] != 1) {
 						adw[temp1][temp2] = aa;
 						mm = false;
 					}
 					
 				}
+				mm = true;
 				
-				
-				adw[temp1][temp2] = aa;
 				
 			}
 			
@@ -291,186 +282,214 @@ public class Number_01 {
 			}
 			
 		}
-		
 		
 		mm = true;
 		temp1 = rd.nextInt(3);
 		temp2 = rd.nextInt(3)+6;  // 0 = 6 1 = 7 2 = 8
 		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] || 
-				adw[temp1][temp2] == adw[temp1][3] || adw[temp1][temp2] == adw[temp1][4] || adw[temp1][temp2] == adw[temp1][5]) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp1 = rd.nextInt(3);
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
+//		
+//		
+//		
+//		
+//			
+//		while(mm) {
+//			
+//			temp1 = rd.nextInt(3);
+//			temp2 = rd.nextInt(3)+6;
+//			if (adw[temp1][temp2] != 1) {
+//				adw[temp1][temp2] = aa;
+//				mm = false;
+//			}
+//		}
+//		
+//		
+//		mm = true;
+//		cnt = 0;
+//		
+//		while(mm) {
+//			
+//			
+//			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] || 
+//				adw[temp1][temp2] == adw[temp1][3] || adw[temp1][temp2] == adw[temp1][4] || adw[temp1][temp2] == adw[temp1][5]) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				
+//				
+//				while(mm) {
+//					adw[temp1][temp2] = 0;
+//					temp1 = rd.nextInt(3);
+//					temp2 = rd.nextInt(3)+6;
+//					
+//					if (adw[temp1][temp2] != 1) {
+//						adw[temp1][temp2] = aa;
+//						mm = false;
+//					}
+//				}
+//				mm = true;
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//		
+//		mm = true;
 		
-		mm = true;
-		temp1 = rd.nextInt(3)+3; // 0 2
-		temp2 = rd.nextInt(3);  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2]) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp2 = rd.nextInt(3);
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-		
-		mm = true;
-		temp1 = rd.nextInt(3)+6; // 0 2
-		temp2 = rd.nextInt(3);  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] || 
-					adw[temp1][temp2] == adw[3][temp2] || adw[temp1][temp2] == adw[4][temp2] || adw[temp1][temp2] == adw[5][temp2]) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp2 = rd.nextInt(3);
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-		
-		mm = true;
-		temp1 = rd.nextInt(3)+6; // 0 1 2 + 6 
-		temp2 = rd.nextInt(3)+3;  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
-				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2]) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp1 = rd.nextInt(3)+6;
-				temp2 = rd.nextInt(3)+3;
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-		
-		mm = true;
-		temp1 = rd.nextInt(3)+6; // 0 1 2 + 6 
-		temp2 = rd.nextInt(3)+6;  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
-				adw[temp1][temp2] == adw[temp1][3] || adw[temp1][temp2] == adw[temp1][4] || adw[temp1][temp2] == adw[temp1][5] ||
-				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp1 = rd.nextInt(3)+6;
-				temp2 = rd.nextInt(3)+6;
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-		
-		mm = true;
-		temp1 = rd.nextInt(3)+3; // 0 1 2 + 6 
-		temp2 = rd.nextInt(3)+6;  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
-				adw[temp1][temp2] == adw[6][temp2] || adw[temp1][temp2] == adw[7][temp2] || adw[temp1][temp2] == adw[8][temp2] ||
-				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp1 = rd.nextInt(3)+3;
-				temp2 = rd.nextInt(3)+6;
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-
-		mm = true;
-		temp1 = rd.nextInt(3)+3; // 0 1 2 + 6 
-		temp2 = rd.nextInt(3)+3;  // 0 = 6 1 = 7 2 = 8
-		
-		adw[temp1][temp2] = aa;
-		while(mm) {
-			cnt = 0;
-			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
-				adw[temp1][temp2] == adw[temp1][6] || adw[temp1][temp2] == adw[temp1][7] || adw[temp1][temp2] == adw[temp1][8] ||
-				adw[temp1][temp2] == adw[6][temp2] || adw[temp1][temp2] == adw[7][temp2] || adw[temp1][temp2] == adw[8][temp2] ||
-				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
-				cnt = 1;
-				System.out.println("°ãÄ§");
-				adw[temp1][temp2] = 0;
-				temp1 = rd.nextInt(3)+3;
-				temp2 = rd.nextInt(3)+3;
-				adw[temp1][temp2] = aa;
-				
-			}
-			
-			if ( cnt == 0) {
-				mm = false;
-			}
-			
-		}
-
-		
-		
-
-		
-		
-		
-		
-		
-			
-		
+//		temp1 = rd.nextInt(3)+3; // 0 2
+//		temp2 = rd.nextInt(3);  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2]) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp2 = rd.nextInt(3);
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//		
+//		mm = true;
+//		temp1 = rd.nextInt(3)+6; // 0 2
+//		temp2 = rd.nextInt(3);  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] || 
+//					adw[temp1][temp2] == adw[3][temp2] || adw[temp1][temp2] == adw[4][temp2] || adw[temp1][temp2] == adw[5][temp2]) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp2 = rd.nextInt(3);
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//		
+//		mm = true;
+//		temp1 = rd.nextInt(3)+6; // 0 1 2 + 6 
+//		temp2 = rd.nextInt(3)+3;  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
+//				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2]) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp1 = rd.nextInt(3)+6;
+//				temp2 = rd.nextInt(3)+3;
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//		
+//		mm = true;
+//		temp1 = rd.nextInt(3)+6; // 0 1 2 + 6 
+//		temp2 = rd.nextInt(3)+6;  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
+//				adw[temp1][temp2] == adw[temp1][3] || adw[temp1][temp2] == adw[temp1][4] || adw[temp1][temp2] == adw[temp1][5] ||
+//				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp1 = rd.nextInt(3)+6;
+//				temp2 = rd.nextInt(3)+6;
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//		
+//		mm = true;
+//		temp1 = rd.nextInt(3)+3; // 0 1 2 + 6 
+//		temp2 = rd.nextInt(3)+6;  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
+//				adw[temp1][temp2] == adw[6][temp2] || adw[temp1][temp2] == adw[7][temp2] || adw[temp1][temp2] == adw[8][temp2] ||
+//				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp1 = rd.nextInt(3)+3;
+//				temp2 = rd.nextInt(3)+6;
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//
+//		mm = true;
+//		temp1 = rd.nextInt(3)+3; // 0 1 2 + 6 
+//		temp2 = rd.nextInt(3)+3;  // 0 = 6 1 = 7 2 = 8
+//		
+//		adw[temp1][temp2] = aa;
+//		while(mm) {
+//			cnt = 0;
+//			if (adw[temp1][temp2] == adw[temp1][0] || adw[temp1][temp2] == adw[temp1][1] || adw[temp1][temp2] == adw[temp1][2] ||
+//				adw[temp1][temp2] == adw[temp1][6] || adw[temp1][temp2] == adw[temp1][7] || adw[temp1][temp2] == adw[temp1][8] ||
+//				adw[temp1][temp2] == adw[6][temp2] || adw[temp1][temp2] == adw[7][temp2] || adw[temp1][temp2] == adw[8][temp2] ||
+//				adw[temp1][temp2] == adw[0][temp2] || adw[temp1][temp2] == adw[1][temp2] || adw[temp1][temp2] == adw[2][temp2] ) {
+//				cnt = 1;
+//				System.out.println("°ãÄ§");
+//				adw[temp1][temp2] = 0;
+//				temp1 = rd.nextInt(3)+3;
+//				temp2 = rd.nextInt(3)+3;
+//				adw[temp1][temp2] = aa;
+//				
+//			}
+//			
+//			if ( cnt == 0) {
+//				mm = false;
+//			}
+//			
+//		}
+//
+//		
+//		
+//
+//		
+//		
+//		
+//		
+//		
+//			
+//		
 				
 				
 		System.out.println("::::::::::::::::::::::::::::::::::::");
